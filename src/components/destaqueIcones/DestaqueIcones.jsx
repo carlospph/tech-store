07edbc97dd411995c5888fdb4863ec2ee6ce7ead
carlospph <intablete@gmail.com>
2025-06-14 03:27:13 -0300

    formatando área icones destaques

--- src/components/destaqueIcones/DestaqueIcones.jsx
+++ src/components/destaqueIcones/DestaqueIcones.jsx
@@ -1,57 +1,101 @@
-import styles from "./DestaqueIcones.module.css";
+import styled from 'styled-components';
+
+const icones = [
+	{
+		id: 1,
+		titulo: "Tênis",
+		image: "/src/assets/icones/tenisAtivo.png",
+	},
+	{
+		id: 2,
+		titulo: "Relógio",
+		image: "/src/assets/icones/relogioAtivo.png",
+	},
+	{
+		id: 3,
+		titulo: "Calça",
+		image: "/src/assets/icones/calcaAtiva.png",
+	},
+	{
+		id: 4,
+		titulo: "Headset",
+		image: "/src/assets/icones/foneAtivo.png",
+	},
+	{
+		id: 5,
+		titulo: "Camiseta",
+		image: "/src/assets/icones/camiseta.png",
+	}
+
+];
+
+const Container = styled.div`
+	display:flex;
+	justify-content:center;
+	gap:24px;
+	margin:0 auto;
+	max-width:660px;
+
+		@media (max-width: 769px){
+			overflow: hidden;
+			overflow-x: auto;
+			padding:1rem 1.5em 1rem 8em;
+		}
+ `;
+
+const Card = styled.div`
+ 	display:flex;
+	flex-direction:column;
+	align-items:center;
+	justify-content:center;
+	width:fit-content;
+	gap: 5px;
+`;
+
+const Title = styled.h5`
+	padding: 6px 0 0 0;
+	font-size:14px;
+`;
+
+const Bodycard = styled.div`
+	border:2px solid #ccc;
+	padding:1rem;
+	border-radius: 50%;
+	cursor:pointer;
+
+	img{
+		width:54px;
+		height:auto;
+		width:50px;
+		opacity:.6;
+	}
+
+	&:hover{
+		transition: .4s all;
+		border-color:blueviolet;
+
+			img{
+				opacity:1;
+			}
+	}
+`;
+
 
 export const DestaqueIcones = () => {
 	return (
-		<div className={styles.destaqueIcones}>
-			
-			<div className={styles.card__icone}>
-				<div className={styles.icone}>
-					<img src="/src/assets/icones/tenisAtivo.png" className={styles.card__img} alt="Icone do tênis" />
-				</div>
-				<div>
-					<h5 className={styles.card__title}>Tênis</h5>
-				</div>
-			</div>
 
-			<div className={styles.card__icone}>
-				<div className={styles.icone}>
-					<img src="/src/assets/icones/calcaAtiva.png" className={styles.card__img} alt="Icone da Calça" />
-				</div>
-				<div>
-					<h5 className={styles.card__title}>Calças</h5>
-				</div>
-			</div>
+		<Container>
+			{icones.map((icone) => (
 
-
-			<div className={styles.card__icone}>
-				<div className={styles.icone}>
-					<img src="/src/assets/icones/relogioAtivo.png" className={styles.card__img} alt="Icone do relógio" />
-				</div>
-				<div>
-					<h5 className={styles.card__title}>Relógio</h5>
-				</div>
-			</div>
-
-
-			<div className={styles.card__icone}>
-				<div className={styles.icone}>
-					<img src="/src/assets/icones/foneAtivo.png" className={styles.card__img} alt="Icone headset" />
-				</div>
-				<div>
-					<h5 className={styles.card__title}>Headset</h5>
-				</div>
-			</div>
-
-			<div className={styles.card__icone}>
-				<div className={styles.icone}>
-					<img src="/src/assets/icones/camiseta.png" className={styles.card__img} alt="Icone de camisa" />
-				</div>
-				<div>
-					<h5 className={styles.card__title}>Tshirt</h5>
-				</div>
-			</div>
-
-			
-		</div>
+				<Card key={icone.id}>
+					<Bodycard>
+						<img src={icone.image} alt={icone.titulo} />
+					</Bodycard>
+					<Title>
+						{icone.titulo}
+					</Title>
+				</Card>
+			))}
+		</Container>
 	);
 };
